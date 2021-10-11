e73f199f6dfd06ddfb8448eb66b199ceb2274c4a
Ty Cobb <ty.cobb.m@gmail.com>
2021-10-10 23:07:49 -0400

    messy attack

--- Assets/Unknown/Player/Player.cs
+++ Assets/Unknown/Player/Player.cs
@@ -1,12 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 /// a player
 public class Player: MonoBehaviour {
+    // -- types --
+    struct Release {
+        /// the flick offset on release
+        Vector2 Offset;
+        /// the time on release
+        float Time;
+    }
     // -- tuning --
+    [FormerlySerializedAs("mFlickLength")]
     [Header("tuning")]
-    [Tooltip("the maximum length of a flick")]
-    [SerializeField] float mFlickLength = 0.1f;
+    [Tooltip("the max length of the flick windup")]
+    [SerializeField] float mWindupLength = 0.1f;
+
+    [Tooltip("the curve for the flick release")]
+    [SerializeField] AnimationCurve mReleaseCurve;
+
+    [Tooltip("the duration for the flick release")]
+    [SerializeField] float mReleaseDuration = 0.1f;
+
+    [Tooltip("the min alignment to detect a flick release")]
+    [SerializeField] float mReleaseAlignment = 0.1f;
 
     // -- nodes --
     [Header("nodes")]
@@ -35,11 +53,14 @@ public class Player: MonoBehaviour {
     /// the line pattern
     Pattern mPattern;
 
-    /// the flick offset
-    Vector2 mFlickOffset;
+    /// the flick windup offset
+    Vector2 mWindupOffset;
 
-    /// the flick speed
-    Vector2 mFlickSpeed;
+    /// the flick offset when released
+    Vector2 mReleaseOffset;
+
+    float? mReleaseTime;
+    float mReleaseMax;
 
     /// the player's inputs
     PlayerActions mActions;
@@ -49,13 +70,16 @@ public class Player: MonoBehaviour {
         // set props
         mPattern = new Pattern();
         mActions = new PlayerActions(mInput);
+        mReleaseMax = mReleaseCurve.keys[1].value - 1.0f;
     }
 
     void Update() {
         // update state from input
         ReadMove();
         ReadFlick();
+    }
 
+    void FixedUpdate() {
         // move line
         Move();
     }
@@ -84,11 +108,26 @@ public class Player: MonoBehaviour {
     /// read flick offset and velocity
     void ReadFlick() {
         // capture prev and next offset
-        var prev = mFlickOffset;
+        var prev = mWindupOffset;
         var next = mActions.Flick;
 
-        // update state
-        mFlickOffset = next;
+        // update windup state
+        mWindupOffset = next;
+
+        // given the stick delta
+        var delta = next - prev;
+
+        // get alignment with previous frame
+        var pDir = prev.normalized;
+        var rDir = delta.normalized;
+        var align = Vector2.Dot(pDir, rDir);
+
+        // if the stick was released
+        if (mReleaseTime == null && Mathf.Abs(align + 1.0f) < mReleaseAlignment) {
+            mReleaseTime = Time.time;
+            mReleaseOffset = prev;
+            mReleaseCurve.keys[1].value = 1.0f + mReleaseMax * delta.magnitude;
+        }
     }
 
     /// move line into position
@@ -97,8 +136,25 @@ public class Player: MonoBehaviour {
         var p0 = mPattern.Point0;
         var p1 = mPattern.Point1;
 
+        // the flick offset
+        var offset = Vector2.zero;
+        if (mReleaseTime != null) {
+            var elapsed = Time.time - mReleaseTime.Value;
+
+            if (elapsed > mReleaseDuration) {
+                mReleaseTime = null;
+            } else {
+                var curved = mReleaseCurve.Evaluate(elapsed / mReleaseDuration);
+                offset = Vector2.LerpUnclamped(mReleaseOffset, Vector2.zero, curved);
+            }
+        }
+
+        if (mReleaseTime == null) {
+            offset = mWindupOffset;
+        }
+
         // the flick-adjusted endpoint
-        var pe = p1 + mFlickOffset * mFlickLength;
+        var pe = p1 + offset * mWindupLength;
 
         // render shapes
         mLine.Start = p0;
